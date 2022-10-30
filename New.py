9bbd7c3721cda4fa02496dbc41cbd61efde8e76f
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-10-30 16:09:48 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,17 @@
+
+mystr ="hello world"
+print(mystr[0:7])
+print(mystr[1])
+print(len(mystr))
+print(type(mystr))
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.endswith("b"))
+print(mystr.startswith("h"))
+print(mystr.count("l"))
+print(mystr.capitalize())
+print(mystr.find("world"))
+print(mystr.upper())
+print(mystr.lower())
+print(mystr.replace("world", "python"))
+
