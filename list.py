6dd77c595f85a55b746f0959ebff0c0531f21d26
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-10-30 16:50:13 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+"""List"""#can change
+mylist = ["sanjay","vijay","ajay","ram",100]
+#print(mylist)
+##print(mylist[4])
+#print(mylist[:])
+number = [2,2,7,56,3,2,6,8,9]
+#number.sort()
+#number.reverse()
+#print(number[::])
+#print(max(number))
+#print(min(number))
+#number.append(7)
+
+#number.insert(2,67)
+#number.remove(56)
+#number.pop()
+#number.push(9)
+#number[1]=10
+#print(number)
+"""tuple"""#can not change
+
+tuple=(1,2,3)
+tuple[10]=1
+print(tuple)
+
