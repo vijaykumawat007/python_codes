971bfed070b258c27d5e6a9e8918aa6faef4c460
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-01 20:29:38 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,13 @@
+s= set()
+#print(type(s))
+#s_from_list = set([1,2,3,4])
+#print(s_from_list)
+s.add(1)
+s.add(2)
+s.add(3) 
+#s1=s.union({1,2,3,4,10,4})
+#s1=s.intersection({1,2,3,4})
+#print(s,s1)
+s1={4,6}
+print(s.isdisjoint(s1))
+#print(type(s))
