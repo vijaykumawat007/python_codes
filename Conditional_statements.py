ea5355a166bfd797172d93cb8b895728cb97d83c
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-07 10:37:04 +0530

    Add files via upload

---
+++ Conditional_statements.py
@@ -0,0 +1,27 @@
+"""
+var1 = 6
+var2 = 56
+var3 = int(input())
+if var3>var2:
+    print("greater")
+#else if
+elif var3==var2:
+    print("equal")
+else:
+    print("lesser")
+"""
+"""
+list =[1,2,3,4,5,6]
+if 7 in list:
+    print("it is in")
+else:
+    print("it is not in it")
+"""
+print("the value of age for driving")
+Var1 = int(input())
+if Var1==18:
+    print("the person is eligible for driving license")
+elif Var1>18:
+    print("the person can drive vhicle")
+else:
+    print("the person is not eligible for driving")
