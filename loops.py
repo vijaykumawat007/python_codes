569460dcb00c71a0f6f85874d3ef5ff86563834d
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-07 11:22:27 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,20 @@
+#For Loop
+"""
+list1 = [["vijay",1],["sanjay",2],["ajay",3],["sachin",4]]
+dict1=dict(list1)
+for item in dict1:
+    print(item)
+#print(dict1)
+#for item,lollypop in dict1.items():
+#    print(item,"has",lollypop,"Lollypop")
+
+List1= ["ram",33,"raju","kaju",87,567,55,557,1,111]
+for item in List1:
+    if str(item).isnumeric()and item>34:
+        print("value is",item)
+"""
+#while loop
+i=0
+while(i<45):
+    print("ram")
+    i=i+1
