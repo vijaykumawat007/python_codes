408a6ed080a5158b86de34502f58094865337bf4
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-19 16:01:54 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,57 @@
+#Arithmatic operator
+"""
+print("5+6 is ", 5+6)
+print("5-6 is ", 5-6)
+print("5*6 is ", 5*6)
+print("5/6 is ", 5/6)
+#//floor division operator
+print("15//6 is ", 15//6)
+#**exponent operator
+print("5**6 is ", 5**6)
+print("5%5 is ", 5%5)
+"""
+
+#Assignment operator
+
+"""
+print("assignment opertor")
+x=5
+print(x)
+x+=7
+print(x)
+"""
+
+#comparison operator
+
+"""
+i =5
+print(i<=5)
+print(i==5)
+print(i>=6)
+"""
+
+#logical operator
+"""
+a = True
+b = False
+print(a is b)
+print(a is not b)
+"""
+
+#identity operator
+"""
+print(5 is not 5)
+"""
+
+#membership operator
+"""
+list = [3,4,5,6,7,10,100,160,200]
+print(170 not in list)
+print(100 in list)
+"""
+
+#bitwise operator
+"""
+print(0 & 1)
+print(0 | 1)
+"""
