5031c2582b19366eec9d26d98a66afac4d252596
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-19 17:03:35 +0530

    Add files via upload

---
+++ try_except.py
@@ -0,0 +1,12 @@
+print("enter first value")
+num1 = input()
+print("enter second value")
+num2 = input()
+
+try:
+    print("The sum of these two n0.",int(num1)+int(num2))
+
+except Exception as e:
+    print(e)
+
+print("this is very important")
