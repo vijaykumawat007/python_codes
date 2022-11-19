cfadb42f4c7866dc4c5b3e38d5a0d00e3f580f0d
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-19 17:44:30 +0530

    Add files via upload

---
+++ read_file.py
@@ -0,0 +1,17 @@
+f = open("file.txt","rt")
+#content = f.read()
+#print(content)
+#print(f.readline())""" it is print a particular line """
+#print(f.readline())
+#print(f.readlines()) """ it is print the list of lines"""
+#for line in f:
+#    print(line, end="")
+#content = f.read(100)
+#print("1",content)
+
+#content = f.read(100)
+#print("2",content)
+f.close()
+
+
+
