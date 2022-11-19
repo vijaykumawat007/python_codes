2f3a3b0f59415b17430f4e1321c7f94443a30183
Vijay Kumawat <67304529+vijaykumawat007@users.noreply.github.com>
2022-11-19 16:43:38 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+"""
+a = 9
+b = 8
+c = sum((a, b)) #built in function
+"""
+"""
+def fun1(a,b):
+    average = (a+b)/2
+    print(average)
+fun1(9, 8)
+"""
+def add(a,b):
+    """this is my function to perform addition of two no.
+       it print statement about function"""
+    add = a+b
+ #   print(add)
+    return add
+#sum = add(10,20)
+#print(sum)
+print(add.__doc__)# there is print doc string
